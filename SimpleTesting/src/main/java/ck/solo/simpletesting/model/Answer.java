5042f24b3f1024c269d1c9a8087e3b362ae8a5d3
Ihor Shkliar <igor.shklar@gmail.com>
2015-08-05 15:07:59 +0300

    release 04

--- SimpleTesting/src/main/java/ck/solo/simpletesting/model/Answer.java
+++ SimpleTesting/src/main/java/ck/solo/simpletesting/model/Answer.java
@@ -55,7 +55,7 @@ public class Answer implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + id;
+		result = prime * result + getId();
 		return result;
 	}
 
@@ -68,7 +68,7 @@ public class Answer implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Answer other = (Answer) obj;
-		if (id != other.id)
+		if (getId() != other.getId())
 			return false;
 		return true;
 	}
